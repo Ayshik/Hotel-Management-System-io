f387fb94054020f839abd655277bf9a78fbaeeeb
MD WALI MOSNAD AYSHIK <61822224+Ayshik@users.noreply.github.com>
2020-09-18 17:30:30 +0600

    Home page added

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+include('rss/header.php');
+
+
+?>
+
+ <div class="text">
+        <h3>RELAX • Massage • Enjoy</h3>
+        <h1>Paradise's fairy girls</h1>
+        <h2>DON'T Go Anywhere Visit Us!!!</h2>
+        <button id="buttonone" < onclick="window.location.href='#';">
+          Login
+        </button>
+       
+      </div>
+    </div>
+  </body>
+</html>
---
+++ rss/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Pulok er Hotel</title>
+    <link rel="stylesheet" type="text/css" href="css/home.css" />
+    <link
+      href="http://fonts.googleapis.com/css?family=Montserrat:400,700%7CPT+Serif:400,700,400italic"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+    <div class="bgimage">
+      <div class="menu">
+        <div class="leftmenu">
+          <h3><a href="index.php">Moody Moon</a></h3>
+        </div>
+        <div class="rightmenu">
+          <ul>
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Nuru massage</a></li>
+            
+            <li><a href="aboutus.html">About Us</a></li>
+            <li>
+              <a href="contact.html">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
