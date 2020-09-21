df1c91f0477d38b3436af6bb32e3cfe890436271
MD WALI MOSNAD AYSHIK <61822224+Ayshik@users.noreply.github.com>
2020-09-22 02:08:06 +0600

    some update

--- manager/picture_customize.php
+++ manager/picture_customize.php
@@ -8,14 +8,14 @@ if(isset($_SESSION['user_name']))
 {
   if($_SERVER['REQUEST_METHOD']== "POST")
 {
-    
+
     $class = $_POST['class'];
 
     $file_name = $_FILES['room_image'] ['name'];
     $file_tmp_name = $_FILES['room_image'] ['tmp_name'];
     $file_err = $_FILES['room_image'] ['error'];
     $file_size = $_FILES['room_image'] ['size'];
-   
+
     $file_name_ex = explode('.',$file_name);
     $file_ext = $file_name_ex[1];
     $file_ext=strtolower($file_ext);
@@ -43,7 +43,7 @@ if(isset($_SESSION['user_name']))
     {
       $error_message = "image  upload system is down!";
     }
-    
+
 
 }
 }
@@ -64,10 +64,10 @@ body{
 
 .wrapper{
   position: absolute;
-    top: 48%;
-    left: 52%;
+  top: 48%;
+  left: 58%;
     transform: translate(-47%,-50%);
-    max-width: 322px;
+    max-width: 243px;
     width: 109%;
     background: #fff;
     padding: 26px;
@@ -135,8 +135,135 @@ body{
   transition: all 0.5s ease;
 }
 
+/*2nd////////////////////////////////////////////////////////////////////////////////*/
+
+.wrapper2{
+  position: absolute;
+  top: 49%;
+  left: 160%;
+      transform: translate(-47%,-50%);
+      max-width: 244px;
+      width: 109%;
+      background: #fff;
+      padding: 26px;
+      border-radius: 5px;
+      flex: 23px;
+}
+.cpic{
+
+	margin: 0;  /*this margin break the design */
+  padding: 0;
+  box-sizing: border-box;
+  outline: none;
+  font-family: 'Josefin Sans', sans-serif;
+  font-weight: bold;
 
 
+
+}
+.wrapper2 h2{
+  text-align: center;
+  margin-bottom: 20px;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  color: #332902;
+}
+
+.wrapper2 .input_field{
+  margin-bottom: 10px;
+}
+
+.wrapper2 .input_field input[type="text"],.input_field input[type="number"],
+.wrapper2 textarea{
+  border: 1px solid #e0e0e0;
+  width: 100%;
+  padding: 10px;
+}
+
+.wrapper2 textarea{
+  resize: none;
+  height: 80px;
+}
+
+.wrapper2 .btn input[type="submit"]{
+  border: 0px;
+  margin-top: 15px;
+  padding: 10px;
+  text-align: center;
+  width: 100%;
+  background: #fece0c;
+  color: #332902;
+  text-transform: uppercase;
+  letter-spacing: 5px;
+  font-weight: bold;
+  border-radius: 25px;
+  cursor: pointer;
+}
+
+
+.wrapper3{
+  position: absolute;
+    top: 51%;
+    /* left: 160%; */
+    right: 175%;
+    transform: translate(-47%,-50%);
+    max-width: 244px;
+    width: 109%;
+    background: #fff;
+    padding: 36px;
+    border-radius: 5px;
+    flex: 23px;
+}
+.cpic{
+
+	margin: 0;  /*this margin break the design */
+  padding: 0;
+  box-sizing: border-box;
+  outline: none;
+  font-family: 'Josefin Sans', sans-serif;
+  font-weight: bold;
+
+
+
+}
+.wrapper3 h2{
+  text-align: center;
+  margin-bottom: 20px;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  color: #332902;
+}
+
+.wrapper3 .input_field{
+  margin-bottom: 10px;
+}
+
+.wrapper3 .input_field input[type="text"],.input_field input[type="number"],
+.wrapper3 textarea{
+  border: 1px solid #e0e0e0;
+  width: 100%;
+  padding: 10px;
+}
+
+.wrapper3 textarea{
+  resize: none;
+  height: 80px;
+}
+
+.wrapper3 .btn input[type="submit"]{
+  border: 0px;
+  margin-top: 15px;
+  padding: 10px;
+  text-align: center;
+  width: 100%;
+  background: #fece0c;
+  color: #332902;
+  text-transform: uppercase;
+  letter-spacing: 5px;
+  font-weight: bold;
+  border-radius: 25px;
+  cursor: pointer;
+}
 
 </style>
 
@@ -145,12 +272,18 @@ body{
       <div class="main-section">
         <div class="dashbord">
           <div class="icon-section">
-            <div class="wrapper">
 
+
+
+<!--wrapper1------------------------------------------------------------------>
+
+            <div class="wrapper">
+  <form method="post" action="<?php htmlspecialchars($_SERVER['PHP_SELF'])?>" enctype ="multipart/form-data">
 <div class="cpic">
-          <h2>Room Picturre Customize</h2>
+          <h2>Room Picture Customize</h2>
             <div id="error_message" > <?php echo $error_message; ?> </div>
-              <form method="post" action="<?php htmlspecialchars($_SERVER['PHP_SELF'])?>" enctype ="multipart/form-data">
+
+
                 <div class="input_field">
             			<label for="Class">Select Class :</label>
                   <select  name="class"><br><br>
@@ -180,6 +313,108 @@ body{
                 </div>
 
 							</div>
+
+
+
+<!--wrapper2------------------------------------------------------------------>
+
+
+
+
+              <div class="wrapper2">
+
+              <div class="cpic">
+              <h2>Dashboard Picture Customize</h2>
+              <div id="error_message" > <?php echo $error_message; ?> </div>
+
+
+                  <div class="input_field">
+                    <label for="Class">Select Dashboard :</label>
+                    <select  name="class"><br><br>
+
+                      <option value="Owner" selected>Owner</option>
+                      <option value="Admin" >Admin</option>
+                      <option value="Receptionist">Receptionist</option>
+
+                    </select>
+
+                  </div>
+
+
+
+                  <div class="input_field">
+
+                      <label for="pic">Picture :</label >
+                      <input type="file" id="dur" name="room_image">
+
+                  </div>
+
+
+
+
+                  <div class="btn">
+                      <input type="submit" name="dashboard_submit" value="Done">
+                  </div>
+
+                </div>
+
+
+
+<!--wrapper3------------------------------------------------------------------>
+
+
+                <div class="wrapper3">
+
+                <div class="cpic">
+                <h2>Galary Picture Customize</h2>
+                <div id="error_message" > <?php echo $error_message; ?> </div>
+
+
+                <div class="input_field">
+                  <label for="Class">Select Class :</label>
+                  <select  name="class"><br><br>
+
+                    <option value="Premium" selected>Premium</option>
+                    <option value="Economy" >Economy</option>
+                    <option value="Basic">Basic</option>
+
+                  </select>
+
+                </div>
+
+
+
+                    <div class="input_field">
+
+                        <label for="pic">Picture :</label >
+                        <input type="file" id="dur" name="room_image">
+
+                    </div>
+
+
+
+
+                    <div class="btn">
+                        <input type="submit" name="galary_submit" value="Done">
+                    </div>
+
+                  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
               </form>
             </div>
 
