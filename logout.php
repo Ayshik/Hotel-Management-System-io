ef538f4e03fbc2ee2fab56cd144a25d2b34748ad
choyonpk <62893007+choyonpk@users.noreply.github.com>
2020-09-22 19:35:51 +0600

    some error in attendance

--- logout.php
+++ logout.php
@@ -1,6 +1,28 @@
 <?php
-
 session_start();
+include "database/db_connect.php";
+
+if($_SESSION['user_type'] == 2)
+{
+    $uname = $_SESSION['user_name'];
+    $exit_time = date(" h:i:sa ");
+    $date = date(" Y-m-d");
+   
+    $uname = mysqli_real_escape_string($con , $uname);
+    $exit_time = mysqli_real_escape_string($con , $exit_time);
+    $date = mysqli_real_escape_string ($con , $date);
+
+    $sql = "update receptionist_timing set exit_time = '$exit_time' where date = '$date' AND user_name ='$uname'";
+    
+    if(! mysqli_query($con , $sql))
+    {
+        echo "setting time error".mysql_error($con);
+    }
+    
+    
+}
+
+
 
 session_unset();
 session_destroy();
